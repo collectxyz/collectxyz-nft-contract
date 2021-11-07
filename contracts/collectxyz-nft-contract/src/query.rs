364aeb2fb326de490160c306a8708b2dd069bd85
xyz Nuisance <collectxyznuisance@gmail.com>
2021-11-07 11:06:20 -0500

    feat: custom list queries with numeric-id

--- contracts/collectxyz-nft-contract/src/query.rs
+++ contracts/collectxyz-nft-contract/src/query.rs
@@ -1,7 +1,7 @@
 use rsa::pkcs8::ToPublicKey;
 
 use collectxyz::nft::{
-    full_token_id, Config, Coordinates, Cw721AllNftInfoResponse, Cw721NftInfoResponse,
+    full_token_id, numeric_token_id, Config, Coordinates, Cw721AllNftInfoResponse, Cw721NftInfoResponse, Cw721TokensResponse,
     MoveParamsResponse, QueryMsg, XyzExtension, XyzTokenInfo, XyzTokensResponse,
 };
 use cosmwasm_std::{to_binary, Binary, BlockInfo, Deps, Empty, Env, Order, StdError, StdResult};
@@ -129,6 +129,14 @@ pub fn cw721_base_query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary
             full_token_id(token_id),
             include_expired.unwrap_or(false),
         )?),
+        Cw721QueryMsg::Tokens {
+            owner,
+            start_after,
+            limit,
+        } => to_binary(&query_tokens(deps, owner, start_after, limit)?),
+        Cw721QueryMsg::AllTokens { start_after, limit } => {
+            to_binary(&query_all_tokens(deps, start_after, limit)?)
+        }
         _ => cw721_contract.query(deps, env, cw721_msg),
     }
 }
@@ -152,6 +160,49 @@ pub fn query_all_nft_info(
         },
         info: info.as_cw721_nft_info(),
     })
+}
+
+pub fn query_tokens(
+    deps: Deps,
+    owner: String,
+    start_after: Option<String>,
+    limit: Option<u32>,
+) -> StdResult<Cw721TokensResponse> {
+    let limit = limit.unwrap_or(DEFAULT_LIMIT).min(MAX_LIMIT) as usize;
+    let start = start_after.map(Bound::exclusive);
+
+    let owner_addr = deps.api.addr_validate(&owner)?;
+    let pks: Vec<_> = tokens()
+        .idx
+        .owner
+        .prefix(owner_addr)
+        .keys(deps.storage, start, None, Order::Ascending)
+        .take(limit)
+        .collect();
+
+    let res: Result<Vec<_>, _> = pks.iter().map(|v| String::from_utf8(v.to_vec())).collect();
+    let tokens = res.map_err(StdError::invalid_utf8)?;
+    let numeric_tokens: Vec<String> = tokens.iter().map(|s| numeric_token_id(s.to_string())).collect();
+    Ok(Cw721TokensResponse { tokens: numeric_tokens })
+}
+
+pub fn query_all_tokens(
+    deps: Deps,
+    start_after: Option<String>,
+    limit: Option<u32>,
+) -> StdResult<Cw721TokensResponse> {
+    let limit = limit.unwrap_or(DEFAULT_LIMIT).min(MAX_LIMIT) as usize;
+    let start = start_after.map(Bound::exclusive);
+
+    let res: StdResult<Vec<String>> = tokens()
+        .range(deps.storage, start, None, Order::Ascending)
+        .take(limit)
+        .map(|item| item.map(|(k, _)| String::from_utf8_lossy(&k).to_string()))
+        .collect();
+        
+    let tokens = res.map_err(StdError::invalid_utf8)?;
+    let numeric_tokens: Vec<String> = tokens.iter().map(|s| numeric_token_id(s.to_string())).collect();
+    Ok(Cw721TokensResponse { tokens: numeric_tokens })
 }
 
 // adapted from: https://github.com/CosmWasm/cw-nfts/blob/5e1e72a3682f988d4504b94f2e203dd4a5a99ad9/contracts/cw721-base/src/query.rs#L211-L228
